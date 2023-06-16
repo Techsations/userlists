e68bfc2cb13519cfe86aac2f4d50738a22dc29d1
Techsations <akandedamilare.t@gmail.com>
2023-06-16 16:24:20 +0100

    created a table and added a delete button

--- src/Userdata.js
+++ src/Userdata.js
@@ -5,17 +5,17 @@ function userData() {
     const [allStudents, setallStudents] = useState([
         {
             name: "Shola",
-            deparment: "Medicine and Sugery",
+            department: "Medicine and Sugery",
             nationality: "Ukranian"
         },
         {
             name: "Biyaya",
-            deparment: "Computer Science",
+            department: "Computer Science",
             nationality: "South African"
         },
         {
             name: "Adelolu",
-            deparment: "Data Science",
+            department: "Data Science",
             nationality: "Pakistani"
         },
         {
@@ -25,10 +25,57 @@ function userData() {
         }
     ])
 
+    let addStudent = (e) => {
+        e.preventDefault();
+        let updateList = {
+            name: studentName,
+            department: studentDepartment,
+            nationality: studentNationality
+        }
+        console.log(updateList);
+        setallStudents([...allStudents, updateList]);
+    }
+
+    const deleteStudent = (index)=>{
+        console.log(index);
+        const updatedItems = [...allStudents];
+        updatedItems.splice(index, 1);
+        setallStudents(updatedItems);
+        console.log(updatedItems);
+    }
+
+    const [studentName, setstudentName] = useState("")
+    const [studentDepartment, setstudentDepartment] = useState("")
+    const [studentNationality, setstudentNationality] = useState("")
+
+
+
     return (
         <>
             <table class="table table-striped">
-                
+                <thead>
+                    <tr>
+                        <td>S/N</td>
+                        <td>NAME</td>
+                        <td>DEPARTMENT</td>
+                        <td>NATIONALITY</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        allStudents.map((items, i) =>(
+                            <tr>
+                                <td>{i + 1}</td>
+                                <td>{items.name}</td>
+                                <td>{items.department}</td>
+                                <td>{items.nationality}</td>
+                                <td>
+                                    <button onClick={()=>deleteStudent(i)} className='btn btn-danger'>Delete</button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
             </table>
         </>
     )
